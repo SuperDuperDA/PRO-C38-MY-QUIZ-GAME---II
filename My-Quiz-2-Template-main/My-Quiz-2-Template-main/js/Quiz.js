f1b69e7340c9ad6945db2be1cdef7078a94f862b
SuperDuperDA <95718898+SuperDuperDA@users.noreply.github.com>
2022-04-14 11:03:59 +0100

    Add files via upload

---
+++ My-Quiz-2-Template-main/My-Quiz-2-Template-main/js/Quiz.js
@@ -0,0 +1,38 @@
+class Quiz {
+  constructor(){
+    this.start();
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    
+    database.ref('/').update({
+      gameState: state
+    });
+
+  }
+
+ start(){
+    
+      contestant = new Contestant();
+      
+        contestant.getCount();
+      
+      question = new Question()
+      question.display();
+    
+  }
+
+  play(){
+    question.hide();
+    background("Yellow");
+   
+  }
+}
